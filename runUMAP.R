3c6ad57e25c0c222fa13492c187041d9ff5a9e7a
amsszlh <zhanglihua@amss.ac.cn>
2019-09-21 22:33:36 -0700

    add descriptions

--- runUMAP.R
+++ runUMAP.R
@@ -1,3 +1,31 @@
+#' Run UMAP
+# This code is modifed based on Seurat R package
+#' @param data.use input data matrix
+#' @param n.neighbors This determines the number of neighboring points used in
+#' local approximations of manifold structure. Larger values will result in more
+#' global structure being preserved at the loss of detailed local structure. In general this parameter should often be in the range 5 to 50.
+#' @param n.components The dimension of the space to embed into.
+#' @param distance This determines the choice of metric used to measure distance in the input space.
+#' @param n.epochs the number of training epochs to be used in optimizing the low dimensional embedding. Larger values result in more accurate embeddings. If NULL is specified, a value will be selected based on the size of the input dataset (200 for large datasets, 500 for small).
+#' @param learning.rate The initial learning rate for the embedding optimization.
+#' @param min.dist This controls how tightly the embedding is allowed compress points together.
+#' Larger values ensure embedded points are moreevenly distributed, while smaller values allow the
+#' algorithm to optimise more accurately with regard to local structure. Sensible values are in the range 0.001 to 0.5.
+#' @param spread he effective scale of embedded points. In combination with min.dist this determines how clustered/clumped the embedded points are.
+#' @param set.op.mix.ratio Interpolate between (fuzzy) union and intersection as the set operation used to combine local fuzzy simplicial sets to obtain a global fuzzy simplicial sets.
+#' @param local.connectivity The local connectivity required - i.e. the number of nearest neighbors
+#' that should be assumed to be connected at a local level. The higher this value the more connected
+#' the manifold becomes locally. In practice this should be not more than the local intrinsic dimension of the manifold.
+#' @param repulsion.strength Weighting applied to negative samples in low dimensional embedding
+#' optimization. Values higher than one will result in greater weight being given to negative samples.
+#' @param negative.sample.rate The number of negative samples to select per positive sample in the
+#' optimization process. Increasing this value will result in greater repulsive force being applied, greater optimization cost, but slightly more accuracy.
+#' @param a More specific parameters controlling the embedding. If NULL, these values are set automatically as determined by min. dist and spread.
+#' @param b More specific parameters controlling the embedding. If NULL, these values are set automatically as determined by min. dist and spread.
+#' @param seed.use Set a random seed. By default, sets the seed to 42.
+#' @param metric.kwds,angular.rp.forest,verbose other parameters used in UMAP
+#' @export
+#'
 runUMAP <- function(
   data.use,
   n.neighbors = 30L,
